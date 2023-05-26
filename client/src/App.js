9b384f31d236d0d35cab0c6b86153970ac63dc8b
Vivek Bhatt <vivekbhatt3011@gmail.com>
2023-05-26 22:14:03 +0530

    PUshing new data

--- client/src/App.js
+++ client/src/App.js
@@ -17,27 +17,11 @@ import ScanConfirm from './components/HomePage/ScanConfirm/ScanConfirm';
 function App() {
 
   console.log("renderingi n app ", localStorage.getItem('sessionUser'))
-  const PushData=()=> {
-    try {
-      const res=fetch(`${API}/SignUp`, {
-        method: "POST",
-        mode: "cors",
-        headers: {
-          "Content-type": "application/json"
-        }
-      })
-    }
-    catch (e) {
-      console.log(e)
-    }
-  }
+  
   return (
 
     <>
-      <button onClick={()=>{
-        PushData()
-      }}>PUSH</button>
-       {/* <Router>
+       <Router>
           <Routes>
             <Route path="/" element={<HomePage/>}></Route>
             <Route path="signup" element={<SignUp />}></Route>
@@ -48,7 +32,7 @@ function App() {
             <Route path="admin" element={<AdminRoute element={<Admin/>}/>} >  </Route>
             <Route path="QrGenerator" element={<AdminRoute element={<QrGenerator/>}/>} >  </Route>
           </Routes>
-        </Router> */}
+        </Router>
     
     </>
   );
