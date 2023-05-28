27a670f9f34dbc53bf667199d00da68df8a62025
V-A-K-H <128683662+V-A-K-H@users.noreply.github.com>
2023-05-28 23:36:44 +0530

    Update Admin.js

--- client/src/components/AdminProfile/Admin/Admin.js
+++ client/src/components/AdminProfile/Admin/Admin.js
@@ -91,27 +91,27 @@ const Admin = () => {
               <td>{`${new Date(outingInfo.date).getDate()}-${new Date(outingInfo.date).getMonth() + 1}-${new Date(outingInfo.date).getFullYear()}`}</td>
               <td>{outingInfo.purpose}</td>
               let exit = new Date()
-              let exithours =outingInfo.exit.getHours()
-              if(exithours >12){
+              let exithours =new Date(outingInfo.exit).getHours()
+              if(exithours) >12{
               exithours -= 12
               }
               if(exithours <=9){
               exithours = `0${hours}`
               } 
-              let exitmin = outingInfo.exit.getMinutes()
+              let exitmin = new Date(outingInfo.exit).getMinutes()
               if(exitmin <=9){
               exitmin = `0${min}`
               }
               <td>{`${exithours}:${exitmin}`}</td>
               let entry = new Date()
-              let entryhours = outingInfo.exit.getHours()
+              let entryhours = new Date(outingInfo.exit).getHours()
               if(entryhours >12){
                 entryhours -= 12
               }
               if(entryhours <=9){
                 entryhours = `0${hours}`
               }
-              let entrymin = outingInfo.exit.getMinutes()
+              let entrymin = new Date(outingInfo.exit).getMinutes()
               if(entrymin <=9){
                 entrymin = `0${min}`
               }
