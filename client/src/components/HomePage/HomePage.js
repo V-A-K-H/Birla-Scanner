fd4ca720f61789419a9b36b0718602476559e0c5
V-A-K-H <128683662+V-A-K-H@users.noreply.github.com>
2023-05-31 00:41:02 +0530

    Update HomePage.js

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -104,14 +104,14 @@ const HomePage = () => {
         <footer className="footer">
 	<div className="row full-width align-spaced">
     <div className="large-2 medium-3 hide-for-small-only columns">
-    <hr>
+   
       <h4 className="footer-heading">Developers</h4>
     </div>
     <div className="large-5 medium-12 small-12 columns social-container">
-      <a href="#"><img className="social-icon" aria-hidden="true"/></a>
-      <a href="#"><img className="social-icon" aria-hidden="true"/></a>
-      <a href="#"><img className="social-icon" aria-hidden="true"/></a>
-      <a href="#"><img className="social-icon" aria-hidden="true"/></a>
+      <a href="#"><img className="social-icon" src="https://drive.google.com/uc?id=1PtrkJYwYb0StZMDte8xgE-UUN6sIi5Zi" aria-hidden="true"/></a>
+      <a href="#"><img className="social-icon" src="https://drive.google.com/uc?id=1hGmBe8YlXE5OXoFCAlU9R5D_KJ4OOjZa" aria-hidden="true"/></a>
+      <a href="#"><img className="social-icon" src="https://drive.google.com/uc?id=12orR1VZ_dIS2B3p7dcAdfsI_8eUA3Evi" aria-hidden="true"/></a>
+      <a href="#"><img className="social-icon" src="https://drive.google.com/uc?id=1xUlHPSPutZu-219bIaDzDYL7DsqKyb0I" aria-hidden="true"/></a>
     </div>
   </div>
 </footer>
