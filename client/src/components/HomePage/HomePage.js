1d794d8438c2cbca96c74361cc8acb2180613e6f
Harshit Roy <101564851+harshit-roy@users.noreply.github.com>
2023-06-27 18:41:28 +0530

    Update HomePage.js
    new changes

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -69,13 +69,13 @@ const HomePage = () => {
   if (userData.access) {
       navigate('/qrscanner')
   }
-  return (
+ return (
     <>
-      <Navbar />
       <body className="homebody">
+        <Navbar />
         <div className="container">
           <div className="profile">
-            <div className="flip-card" onClick={(e) => FlipCard(e)}>
+            <div className="flip-card">
               <div className="flip-card-inner">
                 <div className="flip-card-front">
                   <img
@@ -84,27 +84,133 @@ const HomePage = () => {
                     style={{ height: "300px", width: "300px" }}
                   />
                 </div>
-                <div className="flip-card-back" style={{ color: "white" }}>
-                  <p style={{ paddingTop: "100px" }}>
-                    <h4 style={{ lineHeight: "normal" }}>
-                      {`Name: ${userData.name}`}<br></br>
-                      {`Phone No.: ${userData.phonenum}`}<br></br>
-                      {`Year: ${userData.year}`}
-                    </h4>
-                  </p>
+              </div>
+            </div>
+          </div>
+          <input
+            type="text"
+            className="textbox"
+            placeholder="Purpose"
+            value={purpose}
+            onChange={(e) => {
+              setPurpose(e.target.value);
+            }}
+          />
+          <Link to="qrscanner" state={{ purpose: purpose }}>
+            <button
+              disabled={purpose ? false : true}
+              style={{ backgroundColor: purpose ? "blue" : "black" }}
+              className="buttonqr"
+            >
+              {" "}
+              Open QR Scanner
+            </button>
+          </Link>
+        </div>
+        <div className="footer" style={{ marginTop: "70px" }}>
+          <div class="container">
+            <div className="footer-cta pt-5 pb-5">
+              <div className="row">
+                <div className="col-xl-12 col-lg-12 mb-50">
+                  <div className="footer-widget">
+                  <div className="footer-logo">
+                      <img
+                        src="https://drive.google.com/uc?id=1Rtbcsaaf63qnF5GI1rVnbz6Wzk_HxOkQ"
+                        className="img-fluid"
+                        alt="logo"
+                      />
+                    </div>
+                    <div className="footer-text">
+                      <p>
+                        The security system uses QR code scanning to track the
+                        entry and exit of students in college, maintaining a
+                        record of their in and out history for monitoring and
+                        security purposes.
+                      </p>
+                    </div>
+                    <div className="footer-social-icon">
+                      <span>Developers</span>
+                      <a href="#">
+                        <img
+                          className="dev-photo"
+                          src="https://drive.google.com/uc?id=12orR1VZ_dIS2B3p7dcAdfsI_8eUA3Evi"
+                        />
+                      </a>
+                      <a href="#">
+                        <img
+                          className="dev-photo"
+                          src="https://drive.google.com/uc?id=1xUlHPSPutZu-219bIaDzDYL7DsqKyb0I"
+                        />
+                      </a>
+                      <a href="#">
+                        <img
+                          className="dev-photo"
+                          src="https://drive.google.com/uc?id=1PtrkJYwYb0StZMDte8xgE-UUN6sIi5Zi"
+                        />
+                      </a>
+                      <a href="#">
+                        <img
+                          className="dev-photo"
+                          src="https://drive.google.com/uc?id=1hGmBe8YlXE5OXoFCAlU9R5D_KJ4OOjZa"
+                        />
+                      </a>
+                    </div>
+                    
+                  </div>
+                </div>
+                <div
+                  class="col-xl-12 col-md-12 mb-30"
+                  style={{ marginTop: "50px" }}
+                >
+                  <div class="single-cta">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <div class="cta-text">
+                      <h4>
+                        Find us
+                        <br />
+                        <h5>B3 Top Floor BIAS Bhimtal</h5>
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+                {/* <div className="col-xl-4 col-md-4 mb-30">
+                  <div className="single-cta">
+                    <i className="fas fa-phone"></i>
+                    <div className="cta-text">
+                      <h4>Call us</h4>
+                      <span></span>
+                    </div>
+                  </div>
+                </div> */}
+                <div className="col-xl-6 col-md-6 mb-30">
+                  <div className="single-cta">
+                    <i className="far fa-envelope-open"></i>
+                    <div className="cta-text">
+                      <h4>
+                        Mail us
+                        <br />
+                        <h5>dwarpalsystem@gmail.com</h5>
+                      </h4>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-          <textarea type="text" className="textbox" placeholder="Purpose" value={purpose} onChange={(e) => { setPurpose(e.target.value) }} />
-          <Link to="qrscanner" state={{ purpose: purpose }}>
-            <button disabled={purpose ? false : true} style={{ backgroundColor: purpose ? "#088395" : "black" }} className="buttonqr"> Open QR Scanner</button>
-          </Link>
+          <div className="copyright-area">
+            <div className="container">
+              <div className="row">
+                <div className="col-xl-12 col-lg-12 text-center text-lg-left">
+                  <div className="copyright-text">
+                    <p>Copyright &copy; 2023, All Right Reserved by Dwarpal</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </body>
     </>
-
-
   );
 };
 export default HomePage;
